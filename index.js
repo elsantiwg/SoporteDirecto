c6c91222d76b274e672a6318b4ec9aa82dda6ae7
Kevin Santiago Prieto Guerrero <kspg2350@gmail.com>
2025-02-28 22:08:39 -0500

    index js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hola mundo");
+const port = 3000;
+const application = require("./application");
+
+
+application.listen(port, () =>{
+    console.log("Server running at http://localhost" + port);
+})
