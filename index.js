03275ba8cd68a0fd825a5dffbe3e42c186f687ce
Kevin Santiago Prieto Guerrero <kspg2350@gmail.com>
2025-03-03 22:46:32 -0500

    mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const port = 3000;
 const application = require("./application");
-
+const mongoose = require ("mongoose");
+const urlMongoDb = "mongodb+srv://admin:123@soportedirecto.fibfx.mongodb.net/apidb"
 
 application.listen(port, () =>{
     console.log("Server running at http://localhost" + port);
