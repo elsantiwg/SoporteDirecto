7c7e4f9a5f7338627557f284e5ff7f412a750de5
Kevin Santiago Prieto Guerrero <kspg2350@gmail.com>
2025-03-03 22:56:25 -0500

    conexion mongo db

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const port = 3000;
 const application = require("./application");
-const mongoose = require ("mongoose");
-const urlMongoDb = "mongodb+srv://admin:123@soportedirecto.fibfx.mongodb.net/apidb"
+const mongoose = require("mongoose");
+const urlMongoDb = "mongodb+srv://admin:123@soportedirecto.fibfx.mongodb.net/apidb";
 
-application.listen(port, () =>{
-    console.log("Server running at http://localhost" + port);
-})
+async function startServer() {
+  try {
+    // Conectar a MongoDB
+    await mongoose.connect(urlMongoDb);
+    console.log("La conexión a la base de datos es correcta");
+
+    // Iniciar el servidor
+    application.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+  }
+}
+
+// Llamar a la función para iniciar el servidor
+startServer();
