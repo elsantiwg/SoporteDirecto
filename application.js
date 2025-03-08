95b38b89eaccc386d7468d76e927d4242229d9bb
Kevin Santiago Prieto Guerrero <kspg2350@gmail.com>
2025-03-07 23:31:29 -0500

    modificaciones

--- application.js
+++ application.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const application = express()
 
+application.use(express.json());
+application.use(express.urlencoded({extended:true}));
+
 module.exports = application;
+
+const problems_routes = require("./routes/problems")
+
+application.use("/api", problems_routes);
